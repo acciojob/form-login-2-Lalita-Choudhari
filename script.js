96f9fc5be236e3c2e470f4f3d880067b85a22a14
Lalita-Choudhari <147739736+Lalita-Choudhari@users.noreply.github.com>
2024-04-05 16:35:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function submitForm() {
+  var firstName = document.getElementById("firstName").value;
+  var lastName = document.getElementById("lastName").value;
+  var email = document.getElementById("email").value;
+	var formInfo = "First Name: " + firstName + "\n" +
+                 "Last Name: " + lastName + "\n" +
+		 "Phone Number: " + phone + "\n" +
+                 "Email: " + email + "\n" +
+                 
+  
+  alert(formInfo);
+}
